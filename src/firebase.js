c8ea1cd63e38e60343a0a2767e2d8534f0edd603
高橋凌 <takahashiryou@takahashiryounonotobukkukonpyuta.local>
2026-05-31 00:47:02 +0900

    作業員投稿・情報共有ページ化・ベスト日報移設・月別フィルター・テキスト+ファイル同時投稿

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxoL_tKL9pQoawDCnhSKx3U0s4buRFSx0",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
